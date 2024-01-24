c02d5e38f51bf39c19dc05520b8a8ff1ecded82b
KelseyJoDesign <kelseyjodesign@gmail.com>
2024-01-24 17:46:29 -0600

    no changes commit and push

--- pong_tutorial.py
+++ pong_tutorial.py
@@ -12,7 +12,9 @@ win.bgcolor("navy")
 win.setup(width=800, height=600)
 win.tracer(0)
 
-
+# Score
+score_a = 0
+score_b = 0
 
 # paddle A
 paddle_a = turtle.Turtle()
@@ -42,6 +44,14 @@ ball.goto(0, 0)
 ball.dx = 2
 ball.dy = 2
 
+# keep score with pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Silkscreen", 24, "normal"))
 
 
 # Function of paddles *yay*
@@ -78,6 +88,49 @@ win.onkeypress(paddle_b_down, "Down")
 
 
 
+# Create a screen
+win = turtle.Screen()
+win.title("Yarn Ball")
+win.bgcolor("blue")
+win.setup(width=800, height=600)
+
+# Title
+title = turtle.Turtle()
+title.hideturtle()
+title.penup()
+title.color("white")
+title.goto(0, 150)
+title.write("Yarn Ball", align="center", font=("Arial", 24, "normal"))
+
+# "Go!" Button
+button = turtle.Turtle()
+button.shape("square")
+button.color("purple")
+button.shapesize(stretch_wid=2, stretch_len=3)
+button.penup()
+button.goto(0, -150)
+button_label = turtle.Turtle()
+button_label.hideturtle()
+button_label.penup()
+button_label.goto(0, -160)
+button_label.write("Go!", align="center", font=("Arial", 16, "normal"))
+
+# Function to start the game
+def start_game(x, y):
+    # Hide home screen elements
+    title.clear()
+    button.hideturtle()
+    button_label.clear()
+
+    # Start the game loop
+    # (Place your game loop code here)
+
+# Bind the click event to the "Go!" button
+button.onclick(start_game)
+
+
+
+
 # Main game loop
 while True:
     win.update()
@@ -86,6 +139,8 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
+
+
     # Ball go boyoyoiiing top and bottom
     if ball.ycor() > 290:
         ball.sety(290)
@@ -93,24 +148,35 @@ while True:
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-
+        
     # ball go bye bye left and right
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Silkscreen", 24, "normal"))
+
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Silkscreen", 24, "normal"))
+
+
 
    # Ball bounces off paddle B
     if (ball.xcor() > paddle_b.xcor() - 50 and ball.xcor() < paddle_b.xcor() - 40) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
         ball.setx(paddle_b.xcor() - 50)
         ball.dx *= -1
-
+       
     # Ball bounces off paddle A
     if (ball.xcor() < paddle_a.xcor() + 50 and ball.xcor() > paddle_a.xcor() + 40) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
         ball.setx(paddle_a.xcor() + 50)
         ball.dx *= -1
+    
+
 
     #Control Frame rate from going bonkers
     time.sleep(1/60)
